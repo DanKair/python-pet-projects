6e78831da79311bb97e2bd048d4e7a2e325d800a
DanKair <kair.daniyaruyl@gmail.com>
2024-10-18 16:21:20 +0500

    views.py update1

--- authApp/views.py
+++ authApp/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+# To handle views and redirects
+from django.shortcuts import render, redirect
+# To import auth function from Django
+from django.contrib.auth import authenticate, login, logout
+# The login_required decorator to protect views
+from django.contrib.auth.decorators import login_required
+# For class-based views
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import View
+# import User class model
+from django.contrib.auth.models import User
+from .forms import RegisterForm
 
 # Create your views here.
+
+def register_view(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = User.objects.create_user(username=username, password=password)
+            login(request, user)
+            return redirect('home')
+        else:
+            form = RegisterForm()
+            return render(request,
+             'accounts/register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if User is not None:
+            login(request, user)
+            next_url = request.POST.get('next') or request.GET.get('next') or 'home'
+            return redirect(next_url)
+
+        else:
+            error_message = "Invalid Credentials!"
+    return render(request, 'accounts/login.html', {'error': error_message})
+
+def logout_view(request):
+    pass
+
+# Home View ( Using the decorator)
+@login_required
+def home_view(request):
+    pass
+
